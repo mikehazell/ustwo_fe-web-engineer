f614dd8fc186d8afd46a9fcb2d842f7dd9bc5eff
Mike Hazell <mike.hazell@gmail.com>
2016-08-05 00:42:33 +1000

    create validation unit test

--- tests/validation.js
+++ tests/validation.js
@@ -1,9 +1,20 @@
 import { expect } from 'chai'
-import { hello } from '../src/scripts/hello';
+import { isValidCelebrationType } from '../src/scripts/validations';
 
-describe('HelloWorld test', () => {
-    it('should say "Hello world"', () => {
-        const result = hello();
-        expect(result).to.equal('Hello world');
-    });
+describe('Validation test', () => {
+    describe('isValidCelebrationType')
+        it('should be valid for any value which is not "Other"', () => {
+            const result = isValidCelebrationType('Birthday');
+            expect(result).to.be.true;
+        });
+
+        it('should be valid for "Other" if freetext is given', () => {
+            const result = isValidCelebrationType('Other', 'Something else');
+            expect(result).to.be.true;
+        });
+
+        it('should not be valid for "Other" if no freetext is given', () => {
+            const result = isValidCelebrationType('Other', '');
+            expect(result).to.be.false;
+        });
 })
